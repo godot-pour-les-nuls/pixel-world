a8c960b00b586824cbf3793d023cbbbf7af07b0f
Tom Délié <tomdelie78@gmail.com>
2020-12-30 15:08:35 +0100

    Refactor player HUD and victory screen, add HTML5 export preset

--- HUD.gd
+++ HUD.gd
@@ -5,6 +5,9 @@ var total_chests = 0
 var deaths = 0
 
 onready var player_dash_cooldown = get_parent().get_node("Player/DashCooldown")
+onready var dash_cooldown_texture = $MarginContainer/VBoxContainer/HBoxContainer/DashCooldown/VBoxContainer/TextureProgress
+onready var deaths_counter_number = $MarginContainer/VBoxContainer/HBoxContainer/Counters/VBoxContainer/DeathsCounter/Number
+onready var chests_counter_number = $MarginContainer/VBoxContainer/HBoxContainer/Counters/VBoxContainer/ChestsCounter/Number
 
 func _ready():
 	var chests = get_tree().get_nodes_in_group("chest")
@@ -17,16 +20,16 @@ func _physics_process(delta):
 
 func _on_Chest_collected():
 	chests += 1
-	$ChestsCounter/HBoxContainer/Number.text = String(chests)
+	chests_counter_number.text = String(chests)
 	if chests == total_chests:
 		get_tree().change_scene("res://VictoryScreen.tscn")
 
 func _on_Player_died():
 	deaths += 1
-	$DeathsCounter/HBoxContainer/Number.text = String(deaths)
+	deaths_counter_number.text = String(deaths)
 
 func update_dash_cooldown_hud():
 	var waitTime = player_dash_cooldown.get_wait_time()
 	var timeLeft = player_dash_cooldown.get_time_left()
 	var timeLeftValue = (1 - (timeLeft / waitTime)) * 100
-	$DashInterface/TextureProgress.value = timeLeftValue
+	dash_cooldown_texture.value = timeLeftValue
--- MainMenu.gd
+++ MainMenu.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready():
+	$HUD/MarginContainer/VBoxContainer/Title/AnimationPlayer.play("Bouncing Title")
+
 func _on_StartButton_pressed():
 	Sound.play_click()
 	get_tree().change_scene("res://StageOne.tscn")
